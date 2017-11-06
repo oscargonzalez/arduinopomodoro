2fa9ce33c58a7171c8f689c5c79cb4677e8d0027
oscargonzalez <oscar.gonzalez.varela@gmail.com>
2017-11-06 14:18:25 +0100

    Preliminary working prototype

--- ardupomodoro.ino
+++ ardupomodoro.ino
@@ -12,85 +12,11 @@ char tempString[10];  // Will be used with sprintf to create strings
 int minutes = 25;
 int seconds = 0;
 
-bool pomodoroStarted = false;
+bool pomodoroStarted  = false;
+bool pomodoroPause    = false;
+int  pomodoroCount    = 1;
 
-unsigned int startTime=0;
-
-void updateTimer()
-{
-
-  if (pomodoroStarted == false) { return; }
-  
-  seconds--;
-  if (seconds < 0) { 
-    minutes--;
-    seconds = 59;
-  }
-}
-
-void setup()
-{
-  Wire.begin();  // Initialize hardware I2C pins
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(BUZZER_PIN, OUTPUT);  
-  
-  // Clear the display, and then turn on all segments and decimals
-  clearDisplayI2C();  // Clears display, resets cursor
-
-  // Custom function to send four bytes via I2C
-  //  The I2C.write function only allows sending of a single
-  //  byte at a time.
-  s7sSendStringI2C("8888");
-  //setDecimalsI2C(0b111111);  // Turn on all decimals, colon, apos
-
-  // Flash brightness values at the beginning
-  /*
-  setBrightnessI2C(0);  // Lowest brightness
-  delay(1500);
-  setBrightnessI2C(255);  // High brightness
-  delay(1500);
-  */
-  
-  delay(1000);
-  clearDisplayI2C();  // Clears display, resets cursor
-
-  sprintf(tempString, "%02d%02d", minutes, seconds);
-
-  // This will output the tempString to the S7S
-  s7sSendStringI2C(tempString);
-  
-}
-
-void loop()
-{
-
-  if (digitalRead(BUTTON_PIN) == LOW)
-  {    
-    if (pomodoroStarted == false) { 
-      startTime = millis();
-      pomodoroStarted = true; tone(BUZZER_PIN,1500, 50); 
-    }
-    else {pomodoroStarted = false;  }
-    delay(150); // ugly debounce
-  }
-
-  if (pomodoroStarted == true)
-  {
-    if ( (millis()-startTime) > 1000)
-    {
-      startTime = millis();
-      updateTimer();
-  
-      // Magical sprintf creates a string for us to send to the s7s.
-      //  The %4d option creates a 4-digit integer.
-      sprintf(tempString, "%02d%02d", minutes, seconds);
-    
-      // This will output the tempString to the S7S
-      s7sSendStringI2C(tempString);
-      
-    }
-  }
-}
+unsigned long startTime=0;
 
 // This custom function works somewhat like a serial.print.
 //  You can send it an array of chars (string) and it'll print
@@ -137,4 +63,156 @@ void setDecimalsI2C(byte decimals)
   Wire.write(decimals);
   Wire.endTransmission();
 }
+
+
+void updateTimer()
+{
+
+  if (pomodoroStarted == false) { return; }
+
+  Serial.println("updateTimer");
+   
+  if (seconds == 0) { 
+    if (minutes == 0)
+    {
+      // END COUNTER!
+      if (!pomodoroPause)
+      {
+        tone(BUZZER_PIN, 2500, 50);
+        delay(50);
+        tone(BUZZER_PIN, 1500, 50);
+        delay(50);
+        pomodoroPause = true;
+        minutes = 5;
+        seconds = 0;
+
+        if (pomodoroCount == 4)
+        {
+          // FIN de 4 pomodoros!
+          pomodoroPause = false;
+          pomodoroStarted = false;
+          minutes = 25;
+          seconds = 0;
+          pomodoroCount=1;
+
+          tone(BUZZER_PIN, 500, 200); delay(200);
+          tone(BUZZER_PIN, 1500, 200); delay(200);
+          tone(BUZZER_PIN, 1800, 100); delay(100);
+          tone(BUZZER_PIN, 2500, 300); delay(300);
+          tone(BUZZER_PIN, 2500, 300); delay(400);
+          tone(BUZZER_PIN, 2500, 300); delay(400);
+
+          updateDisplay();
+        }
+      }
+      else
+      {
+        // END pomodoro pause
+        tone(BUZZER_PIN, 1500, 50);
+        delay(50);
+        tone(BUZZER_PIN, 2500, 50);        
+        delay(50);
+        pomodoroPause = false;
+        minutes = 25;
+        seconds = 0;        
+        pomodoroCount++;
+      }      
+    }
+    else
+    {
+      seconds = 59;
+      minutes--;              
+    }
+  }
+  else {
+    seconds--;   
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Setup");
+  Wire.begin();  // Initialize hardware I2C pins
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BUZZER_PIN, OUTPUT);  
+  
+  // Clear the display, and then turn on all segments and decimals
+  clearDisplayI2C();  // Clears display, resets cursor
+  setBrightnessI2C(255);  // High brightness
+  setDecimalsI2C(0b00010000);  // Turn on all decimals, colon, apos
+  sprintf(tempString, "%02d%02d", minutes, seconds);
+  s7sSendStringI2C(tempString);
+
+  setPomodoroPoint(1);
+  
+}
+
+void setPomodoroPoint(int num)
+{
+  if (num == 1) { setDecimalsI2C(0b00011000); }
+  if (num == 2) { setDecimalsI2C(0b00011100); }
+  if (num == 3) { setDecimalsI2C(0b00011110); }
+  if (num == 4) { setDecimalsI2C(0b00011111); }
+}
+
+void updateDisplay()
+{
+  // Magical sprintf creates a string for us to send to the s7s.
+  //  The %4d option creates a 4-digit integer.
+  sprintf(tempString, "%02d%02d", minutes, seconds);
+
+  setDecimalsI2C(0b00011000);
+  
+  // This will output the tempString to the S7S
+  s7sSendStringI2C(tempString);
+  setPomodoroPoint(pomodoroCount);            
+}
+void loop()
+{
+  
+      if(digitalRead(BUTTON_PIN) == LOW)
+      {    
+        Serial.println("Click");        
+        if (pomodoroStarted == false) { 
+          startTime = millis();
+          pomodoroStarted = true; 
+          tone(BUZZER_PIN,1900, 50);   
+          updateTimer();                        
+        }                
+        else
+        {
+          // Cancelado          
+          tone(BUZZER_PIN,1200, 150);   
+          delay(100);
+          tone(BUZZER_PIN,500, 200);   
+          delay(250);
+          pomodoroStarted = false; 
+          pomodoroCount=1;
+          minutes = 25;
+          seconds = 0;          
+          updateDisplay();
+        }
+        delay(250); // crapy debounce
+      }
+    
+      if (pomodoroStarted == true)
+      {
+        Serial.println("update");
+        if ( (millis()-startTime) > 3)
+        {
+          startTime = millis();
+          updateTimer();      
+    
+          Serial.print(minutes);
+          Serial.print(":");
+          Serial.println(seconds);
+      
+          updateDisplay();
+        }
+      }
+      delay(10);
+    }   
+
+
 
